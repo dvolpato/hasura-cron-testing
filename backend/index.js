8b32e4f24bc55f3790b7bdfaf4712fab5c2028ae
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2023-06-23 17:44:10 -0300

    add some mock routes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
 console.log("backend");
 
 const Koa = require('koa');
+const Router = require('@koa/router');
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+} 
+
 const app = new Koa();
 
 // logger
 
 app.use(async (ctx, next) => {
+  console.log(`${ctx.method} ${ctx.url} ${ctx.body ? ctx.body : "{}"}`);
   await next();
   const rt = ctx.response.get('X-Response-Time');
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+  console.log(`${ctx.method} ${ctx.url} ${ctx.body ? ctx.body : "{}"} - ${rt}`);
 });
 
 // x-response-time
@@ -20,9 +27,31 @@ app.use(async (ctx, next) => {
   ctx.set('X-Response-Time', `${ms}ms`);
 });
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const router = new Router();
+router.post("/1", async (ctx) => {
+  await delay(60 * 1000);
+  ctx.status = 200;
 });
+
+router.post("/2", async (ctx) => {
+  console.log('2');
+  await delay(40 * 1000);
+  ctx.status = 200;
+});
+
+router.post("/3", async (ctx) => {
+  console.log('3');
+  await delay(80 * 1000);
+  ctx.status = 200;
+});
+
+router.post("/4", async (ctx) => {
+  console.log('4');
+  await delay(30 * 1000);
+  ctx.status = 200;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
 console.log("backend: listening on port 3000");
