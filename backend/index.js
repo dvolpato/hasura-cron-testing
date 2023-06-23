0a1c870438d57d8e83c32db6c6ef17120537c452
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2023-06-23 20:52:03 -0300

    backend: improve logs

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
-console.log("backend");
+console.log('backend');
 
 const Koa = require('koa');
 const Router = require('@koa/router');
+const bodyParser = require('koa-bodyparser');
 
 function delay(time) {
   return new Promise(resolve => setTimeout(resolve, time));
@@ -9,17 +10,17 @@ function delay(time) {
 
 const app = new Koa();
 
-// logger
+app.use(bodyParser());
 
+// logger
 app.use(async (ctx, next) => {
-  console.log(`${ctx.method} ${ctx.url} ${ctx.body ? ctx.body : "{}"}`);
+  console.log(`${new Date().toISOString()} | ${ctx.method} ${ctx.url}`, ctx.request.body);
   await next();
   const rt = ctx.response.get('X-Response-Time');
-  console.log(`${ctx.method} ${ctx.url} ${ctx.body ? ctx.body : "{}"} - ${rt}`);
+  console.log(`${new Date().toISOString()} | ${ctx.method} ${ctx.url}`, ctx.request.body, `- ${rt}`);
 });
 
 // x-response-time
-
 app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
@@ -28,30 +29,31 @@ app.use(async (ctx, next) => {
 });
 
 const router = new Router();
-router.post("/1", async (ctx) => {
+router.post('/1', async (ctx) => {
+  console.log('/1 - doing some work');
   await delay(60 * 1000);
   ctx.status = 200;
 });
 
-router.post("/2", async (ctx) => {
-  console.log('2');
+router.post('/2', async (ctx) => {
+  console.log('/2 - doing some work');
   await delay(40 * 1000);
   ctx.status = 200;
 });
 
-router.post("/3", async (ctx) => {
-  console.log('3');
+router.post('/3', async (ctx) => {
+  console.log('/3 - doing some work');
   await delay(80 * 1000);
   ctx.status = 200;
 });
 
-router.post("/4", async (ctx) => {
-  console.log('4');
-  await delay(30 * 1000);
+router.post('/4', async (ctx) => {
+  console.log('/4 - doing some work');
+  await delay(10 * 1000);
   ctx.status = 200;
 });
 
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
-console.log("backend: listening on port 3000");
+console.log('backend: listening on port 3000');
